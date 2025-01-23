1bb0d689d47e12d47da07615b085af033ed0793d
Alexander Laptev <notaleksandrlaptev@gmail.com>
2025-01-23 05:23:53 +0300

    Small improvements

--- src/main/java/DefectsDetector.java
+++ src/main/java/DefectsDetector.java
@@ -16,26 +16,19 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class DefectsDetector {
+    private final List<AnalyzerRule> analyzerRules = asList(
+            new ShadowedVariablesAnalyzerRule()
+    );
+
     public boolean detectDefects(Path projectPath) throws IOException {
-        Project project = scanProject(projectPath);
-        ProjectAnalyzer projectAnalyzer = createProjectAnalyzer(project);
+        Project project = new ProjectScanner().scanProject(projectPath);
+        ProjectAnalyzer projectAnalyzer = new ProjectAnalyzer(project, analyzerRules);
         List<AnalyzerDefect> projectDefects = projectAnalyzer.analyze();
         try (Writer output = output()) {
             ProjectDefectsReporter projectDefectsReporter = new ProjectDefectsReporter(project, output);
             projectDefectsReporter.report(projectDefects);
             return !projectDefects.isEmpty();
         }
-    }
-
-    private Project scanProject(Path projectPath) throws IOException {
-        return new ProjectScanner().scanProject(projectPath);
-    }
-
-    private ProjectAnalyzer createProjectAnalyzer(Project project) {
-        List<AnalyzerRule> analyzerRules = asList(
-                new ShadowedVariablesAnalyzerRule()
-        );
-        return new ProjectAnalyzer(project, analyzerRules);
     }
 
     private Writer output() {
